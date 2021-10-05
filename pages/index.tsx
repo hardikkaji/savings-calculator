dddad26398b03508bc64fc6587678d1f0ec08dd1
Hardik Kaji <hardikkaji@gmail.com>
2021-10-06 00:16:52 +0200

    add icon in header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, SvgIcon, Toolbar, Typography } from '@mui/material';
 
 import { Copyright, Calculator } from '../src/components';
 
@@ -13,7 +13,10 @@ const Home: NextPage = () => {
       </Head>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <SvgIcon>
+            <path d="m16 6 2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z" />
+          </SvgIcon>
+          <Typography pl={1} variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Monthly Invesment Calculator
           </Typography>
         </Toolbar>
