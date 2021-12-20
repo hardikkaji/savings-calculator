e8baff16a7111b11d79624a67303e5faf59eb045
Hardik Kaji <hardikkaji@gmail.com>
2021-12-20 23:26:19 +0100

    Add locale support using next-translate

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import { Container, Grid } from '@mui/material';
+import useTranslation from 'next-translate/useTranslation';
+
 import { Result, SliderInput, PieChart } from '..';
 
 export const Calculator = () => {
+  const { t } = useTranslation('home');
   return (
     <Container maxWidth="md">
       <Grid container pt={4} spacing={4}>
-        <Grid item flex="1" md={8} xs={12} sm={8}>
+        <Grid item flex="1" md={6} xs={12} sm={6}>
           <SliderInput
-            label="Starting amount"
+            label={t('startingAmount')}
             endAdornment="kr"
             max={1000000}
             step={500}
             fieldName="startingAmount"
           />
           <SliderInput
-            label="Investment per month"
+            label={t('investmentPerMonth')}
             endAdornment="kr"
             max={100000}
             step={500}
             fieldName="investedAmount"
           />
-          <SliderInput fieldName="expectedReturn" label="Expected Return Rate" endAdornment="%" />
+          <SliderInput fieldName="expectedReturn" label={t('expectedReturn')} endAdornment="%" />
           <SliderInput
             fieldName="timePeriod"
-            label="Time Period"
+            label={t('timePeriod')}
             endAdornment="Yrs"
             min={1}
             max={50}
           />
         </Grid>
-        <Grid item md={4} xs={12} sm={4}>
+        <Grid item md={6} xs={12} sm={6}>
           <PieChart />
         </Grid>
       </Grid>
--- src/components/PieChart/PieChart.tsx
+++ src/components/PieChart/PieChart.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { Grid, Typography, useTheme } from '@mui/material';
 import { PieChart as PChart } from 'react-minimal-pie-chart';
 import { Box } from '@mui/system';
+import useTranslation from 'next-translate/useTranslation';
 
 import { useCalculateGain } from '../../hooks';
 
 export const PieChart = () => {
   const { totalInvestment, wealthGained } = useCalculateGain();
+  const { t } = useTranslation('home');
   const theme = useTheme();
   return (
     <>
       <Grid container direction="row" justifyContent="center" alignItems="center">
         <Grid item mr={1}>
-          <Box sx={{ width: 25, height: 10, backgroundColor: theme.palette.info.main }} />
+          <Box sx={{ width: 20, height: 10, backgroundColor: theme.palette.info.main }} />
         </Grid>
         <Grid item mr={1}>
-          <Typography variant="subtitle2">Total Investment</Typography>
+          <Typography variant="subtitle2">{t('totalInvested')}</Typography>
         </Grid>
         <Grid item mr={1}>
-          <Box sx={{ width: 25, height: 10, backgroundColor: theme.palette.primary.main }} />
+          <Box sx={{ width: 20, height: 10, backgroundColor: theme.palette.primary.main }} />
         </Grid>
         <Grid item>
-          <Typography variant="subtitle2">Est. Returns</Typography>
+          <Typography variant="subtitle2">{t('estReturns')}</Typography>
         </Grid>
       </Grid>
       <PChart
@@ -29,8 +31,8 @@ export const PieChart = () => {
         lineWidth={35}
         paddingAngle={5}
         data={[
-          { title: 'Total Investment', value: totalInvestment, color: theme.palette.info.main },
-          { title: 'Est. Returns', value: wealthGained, color: theme.palette.primary.main },
+          { title: t('totalInvested'), value: totalInvestment, color: theme.palette.info.main },
+          { title: t('estReturns'), value: wealthGained, color: theme.palette.primary.main },
         ]}
       />
     </>
